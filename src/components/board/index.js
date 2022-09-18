220d5adc40735a68c869645d0f08021be16f5e37
shashank rai <shashank.rai00331@gmail.com>
2022-09-18 15:35:05 +0530

    changes added

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,44 +1,58 @@
-import React, { useState } from 'react';
-import Styles from './index.module.css';
+import React, { useEffect, useRef, useState } from "react";
+import Styles from "./index.module.css";
 
 export default function Board() {
-  const [inputlist, setlist] = useState('taskA');
-  const [input, setinput] = useState([]);
-
-  const modaltoggle = (event) => {
-    setlist(event.target.value);
-  };
+  const [inputlist, setlist] = useState(
+    localStorage.getItem("projectTasks")
+      ? JSON.parse(localStorage.getItem("projectTasks"))
+      : []
+  );
+  const [input, setinput] = useState("");
 
   const modalchange = () => {
-    setinput((olditems) => {
-      return [...olditems, inputlist];
-    });
+    if (input === "") alert("Empty Fields");
+    else {  
+      localStorage.setItem("projectTasks", JSON.stringify([...inputlist, input]));
+      setlist((olditems) => {
+        return [...olditems, input];
+      });
+      setinput("");
+    }
   };
 
+  const deleteItem = (indexGiven) => {
+      const newData = inputlist.filter((item, index) => index!== indexGiven);
+      localStorage.setItem("projectTasks" , JSON.stringify(newData));
+      setlist(newData);
+  }
+
   return (
-    <>
+    <div className={Styles.containerTop}>
       <div className={Styles.wrapper}>
-        <div className={Styles.container}>
+        <div>
           <h1 className={Styles.heading}>My Tasks</h1>
           <button
-            type='button'
+            type="button"
             className={Styles.inputbtn}
             onClick={modalchange}
-          > +
+          >
+            +
           </button>
           <input
             className={Styles.placeholder}
-            type='text'
-            onChange={modaltoggle}
-            placeholder='Add a Task'
+            type="text"
+            onSubmit={modalchange}
+            onChange={(e) => setinput(e.target.value)}
+            value={input}
+            placeholder="Add a task"
           />
           <ul>
-            {input.map((itemval) => {
-              return <li>{itemval}</li>;
+            {inputlist.map((itemval, index) => {
+              return <li key={index}>{itemval} <button onClick={() => deleteItem(index)}>delete</button></li>;
             })}
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 }
