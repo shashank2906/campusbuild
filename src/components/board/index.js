75d5855a525a54aeb5df0930f14d70d93b6979c1
shashank rai <shashank.rai00331@gmail.com>
2022-09-18 20:11:16 +0530

    final commit

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -7,7 +7,7 @@ export default function Board() {
       ? JSON.parse(localStorage.getItem("projectTasks"))
       : []
   );
-  const [input, setinput] = useState("");
+  const [input, setinput] = useState("Add a Task");
 
   const modalchange = () => {
     if (input === "") alert("Empty Fields");
@@ -52,7 +52,52 @@ export default function Board() {
             })}
           </ul>
         </div>
+        
       </div>
+
+      <div className={Styles.bottom}>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Campus Build</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Add Task</label>
+            <input type="text" class="form-control" id="recipient-name" onSubmit={modalchange}
+            onChange={(e) => setinput(e.target.value)}
+            value={input} />
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Details</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" onClick={modalchange} data-bs-dismiss="modal" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+<button type="button" className={Styles.inputbtnbottom} data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">+</button>
+      {/* <button
+            type="button"
+            className={Styles.inputbtnbottom}
+            onClick={modalchange}
+          >
+            +
+          </button> */}
+
+
+      </div>
+
+      
+      
     </div>
   );
 }
